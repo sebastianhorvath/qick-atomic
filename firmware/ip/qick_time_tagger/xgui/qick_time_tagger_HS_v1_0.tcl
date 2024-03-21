ef6771e0759dfbeb19906287a9b7ed5233c7c5e9
hskinker <hskinker@princeton.edu>
2024-03-21 11:40:55 -0400

    Repackaged IP with edge crossing

--- firmware/ip/qick_time_tagger/xgui/qick_time_tagger_HS_v1_0.tcl
+++ firmware/ip/qick_time_tagger/xgui/qick_time_tagger_HS_v1_0.tcl
@@ -11,7 +11,17 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "T_W"
   ipgui::add_param $IPINST -name "FIFO_W"
   ipgui::add_param $IPINST -name "DTR_RST"
+  ipgui::add_param $IPINST -name "ADC_RES" -widget comboBox
 
+}
+
+proc update_PARAM_VALUE.ADC_RES { PARAM_VALUE.ADC_RES } {
+	# Procedure called to update ADC_RES when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.ADC_RES { PARAM_VALUE.ADC_RES } {
+	# Procedure called to validate ADC_RES
+	return true
 }
 
 proc update_PARAM_VALUE.DEBUG { PARAM_VALUE.DEBUG } {
@@ -97,5 +107,10 @@ proc update_MODELPARAM_VALUE.FIFO_W { MODELPARAM_VALUE.FIFO_W PARAM_VALUE.FIFO_W
 proc update_MODELPARAM_VALUE.DTR_RST { MODELPARAM_VALUE.DTR_RST PARAM_VALUE.DTR_RST } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.DTR_RST}] ${MODELPARAM_VALUE.DTR_RST}
+}
+
+proc update_MODELPARAM_VALUE.ADC_RES { MODELPARAM_VALUE.ADC_RES PARAM_VALUE.ADC_RES } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.ADC_RES}] ${MODELPARAM_VALUE.ADC_RES}
 }
 
